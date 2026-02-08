4d6cd6e6d43ee8e25f6c9cc6a5618601ca7b528a
Bogdan <maslyb.tf@gmail.com>
2026-02-08 02:20:07 +0200

    change version

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,12 @@
 fn main() {
+  // Ensure `cargo` rebuilds when app icons change, otherwise `tauri dev` can keep showing
+  // the old/default icon due to build script caching.
+  println!("cargo:rerun-if-changed=tauri.conf.json");
+  println!("cargo:rerun-if-changed=icons/icon.icns");
+  println!("cargo:rerun-if-changed=icons/icon.ico");
+  println!("cargo:rerun-if-changed=icons/32x32.png");
+  println!("cargo:rerun-if-changed=icons/128x128.png");
+  println!("cargo:rerun-if-changed=icons/128x128@2x.png");
+
   tauri_build::build()
 }
